140fd7e0ae0786d7a9b2a08a75571282d9926906
pgm-zegerschaeverbeke <zeger.schaeverbeke@student.arteveldehs.be>
2025-02-18 23:43:34 +0100

    Added partials + layouts + error render

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // import statements
 
 import express from "express";
+import expressLayouts from "express-ejs-layouts";
 import path from "path";
 import { home, about, contact, privacy } from "./controllers/PageController.js";
 
@@ -8,8 +9,12 @@ import { home, about, contact, privacy } from "./controllers/PageController.js";
 // create an instance of express
 const app = express();
 
+// use express layouts
+app.use(expressLayouts);
+
 // set views engine to ejs
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
 app.set("views", path.resolve("src", "views"));
 
 // serve static files from the public folder
@@ -21,6 +26,11 @@ app.get("/", home);
 app.get("/about", about);
 app.get("/contact", contact);
 app.get("/privacy", privacy);
+app.get('*', (req, res) => {
+  res.status(404).render('errors/404', {
+    layout: 'layouts/error',
+});
+});
 
 // start the server, listen on port defined in .env file
 app.listen(process.env.PORT, () => {
