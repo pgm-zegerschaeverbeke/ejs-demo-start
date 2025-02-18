e6af9b09687fc4eecd42ffc10b58a363ee2d8ab0
pgm-zegerschaeverbeke <zeger.schaeverbeke@student.arteveldehs.be>
2025-02-18 17:34:02 +0100

    added a condition and for each

--- src/controllers/PageController.js
+++ src/controllers/PageController.js
@@ -1,27 +1,29 @@
 export const home = (req, res) => {
-  res.render("home", {
+  res.render("pages/home", {
     title: "Dinos are awesome!",
     content: "Dinos are a diverse group of meow meow paw paws"
   });
 };
 
 export const about = (req, res) => {
-  res.render("default", {
+  res.render("pages/default", {
     title: "About us",
     content: "We are a group of dino enthusiasts!",
+    team: ["T-Rex", "Velociraptor", "Stegosaurus"],
   });
 };
 
 export const contact = (req, res) => {
-  res.render("contact", {
+  res.render("pages/contact", {
     title: "Contact",
     content: "You can reach us at 09 233 40 89",
   });
 };
 
 export const privacy = (req, res) => {
-  res.render("default", {
+  res.render("pages/default", {
     title: "Privacy Policy",
     content: "Dinosaurs are protective of their privacy.",
+    team: false,
   });
 };
