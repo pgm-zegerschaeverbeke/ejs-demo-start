8797e0f49373863358c469e5793bb7931ae36373
pgm-zegerschaeverbeke <zeger.schaeverbeke@student.arteveldehs.be>
2025-02-18 16:04:22 +0100

    ejs has been added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 // import statements
 import express from "express";
 import path from "path";
+import { title } from "process";
+
 
 // create an instance of express
 const app = express();
+
+// set views engine to ejs
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("src", "views"));
 
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
@@ -11,7 +17,10 @@ app.use(express.static("public"));
 
 // GET route to serve the index.html file
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src", "views", "index.html"));
+  res.render("home", {
+    title: "Dinos are awesome!",
+    content: "Dinos are a diverse group of meow meow paw paws"
+  })
 });
 
 // start the server, listen on port defined in .env file
