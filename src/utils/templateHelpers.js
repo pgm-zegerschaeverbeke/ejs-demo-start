0e7e8b7804bcbb00e1b1873ed5158e5f610bcb9a
pgm-zegerschaeverbeke <zeger.schaeverbeke@student.arteveldehs.be>
2025-03-04 12:00:42 +0100

    Final changes ejs demo

--- src/utils/templateHelpers.js
+++ src/utils/templateHelpers.js
@@ -4,6 +4,11 @@ const helpers = {
   capitalize: (str) => str.charAt(0).toUpperCase() + str.slice(1),
   truncate: (str, length) =>
     str.length > length ? str.slice(0, length) + "..." : str,
+  highlight: function (inp) {
+    return `<span style="background-color: yellow;">
+      ${inp}
+    </span>`;
+  },
 };
 
 export default helpers;
